6dfe62c5627731c1ad33f9172d60b0a9cb4652da
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2024-07-12 11:42:55 +0200

    Add project files

---
+++ Makefile
@@ -0,0 +1,88 @@
+PACKAGE      	= opsduty_client
+BASE  	     	= $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+POETRY      	= poetry
+
+V = 0
+Q = $(if $(filter 1,$V),,@)
+M = $(shell printf "\033[34;1m▶\033[0m")
+
+.PHONY: all
+all: lint test ; @ ## Lint and test project
+	$Q
+
+$(POETRY): ; $(info $(M) checking POETRY…)
+	$Q
+
+$(BASE): | $(POETRY) ; $(info $(M) checking PROJECT…)
+	$Q
+
+.PHONY: fix
+fix: fix-ruff fix-black | $(BASE) ; @ ## Run all fixers
+	$Q
+
+.PHONY: lint
+lint: lint-ruff lint-black lint-dmypy | $(BASE) ; @ ## Run all linters
+	$Q
+
+.PHONY: test
+test: test-pytest | $(BASE) ; @ ## Run pytest
+	$Q
+
+# Tests
+.PHONY: test-pytest
+test-pytest: .venv | $(BASE) ; $(info $(M) running backend tests…) @ ## Run pytest
+	$Q cd $(BASE) && PYTHONHASHSEED=0 $(POETRY) run pytest
+
+# Linters
+
+.PHONY: lint-black
+lint-black: .venv | $(BASE) ; $(info $(M) running black…) @ ## Run black linter
+	$Q cd $(BASE) && $(POETRY) run black --check tests src
+
+.PHONY: lint-ruff
+lint-ruff: .venv | $(BASE) ; $(info $(M) running ruff…) @ ## Run ruff linter
+	$Q cd $(BASE) && $(POETRY) run ruff check tests src
+
+.PHONY: lint-mypy
+lint-mypy: .venv | $(BASE) ; $(info $(M) running mypy…) @ ## Run mypy linter
+	$Q cd $(BASE) && $(POETRY) run mypy --show-error-codes --show-column-numbers tests src
+
+.PHONY: lint-dmypy
+lint-dmypy: .venv | $(BASE) ; $(info $(M) running mypy…) @ ## Run dmypy linter
+	$Q cd $(BASE) && $(POETRY) run -- dmypy run tests src -- --show-error-codes
+
+# Fixers
+
+.PHONY: fix-black
+fix-black: .venv | $(BASE) ; $(info $(M) running black…) @ ## Run black fixer
+	$Q cd $(BASE) && $(POETRY) run black tests src
+
+.PHONY: fix-ruff
+fix-ruff: .venv | $(BASE) ; $(info $(M) running ruff…) @ ## Run ruff fixer
+	$Q cd $(BASE) && $(POETRY) run ruff check --fix tests src
+
+# Release
+.PHONY: release
+release: all ; $(info $(M) running release…) @ ## Run poetry release
+	$Q cd $(BASE) && $(POETRY) build
+	$Q cd $(BASE) && $(POETRY) publish
+
+# Dependency management
+
+.venv: pyproject.toml poetry.lock | $(BASE) ; $(info $(M) retrieving dependencies…) @ ## Install python dependencies
+	$Q cd $(BASE) && $(POETRY) run pip install -U pip
+	$Q cd $(BASE) && $(POETRY) install
+	@touch $@
+
+# Misc
+
+.PHONY: clean
+clean: ; $(info $(M) cleaning…) @ ## Cleanup caches and virtual environment
+	@rm -rf .eggs *.egg-info .venv test-reports
+	@find . -type d -name '__pycache__' -exec rm -rf {} +
+	@find . -type d -name '*pytest_cache*' -exec rm -rf {} +
+
+.PHONY: help
+help: ## This help message
+	@grep -E '^[ a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | \
+		awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' | sort
---
+++ tests/heartbeats/test_heartbeats.py
@@ -0,0 +1,85 @@
+import pytest
+from opsduty_client.heartbeats.heartbeats import (
+    heartbeat_checkin,
+    requests_retry_session,
+    send_heartbeat_checkin,
+)
+from opsduty_client.settings import settings
+from pytest_mock import MockerFixture
+from requests import Session
+from responses import RequestsMock
+from urllib3.util.retry import Retry
+
+
+def test_requests_retry_session() -> None:
+    """Make sure retries are configured correctly."""
+
+    retries = 10
+    backoff_factor = 10.0
+    status_forcelist = (500,)
+
+    session = requests_retry_session(
+        session=Session(),
+        retries=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+
+    https_adapter = session.adapters["https://"]
+    retry: Retry = https_adapter.max_retries
+
+    assert retry.total == retries
+    assert retry.connect == retries
+    assert retry.read == retries
+    assert retry.backoff_factor == backoff_factor
+    assert retry.status_forcelist == status_forcelist
+
+
+@pytest.mark.parametrize("enabled", (True, False))
+@pytest.mark.parametrize("job_raise_error", (True, False))
+@pytest.mark.parametrize("environment", ("prod", None))
+def test_heartbeat_checkin(
+    enabled: bool, job_raise_error: bool, environment: str | None, mocker: MockerFixture
+) -> None:
+    """Make sure the decorator calls send_heartbeat_checkin when it should."""
+
+    mock = mocker.patch("opsduty_client.heartbeats.heartbeats.send_heartbeat_checkin")
+
+    @heartbeat_checkin(heartbeat="test", environment=environment, enabled=enabled)
+    def job() -> None:
+        if job_raise_error:
+            raise RuntimeError("Job failed")
+
+    if job_raise_error:
+        with pytest.raises(RuntimeError):
+            job()
+    else:
+        job()
+
+    if enabled and not job_raise_error:
+        mock.assert_called_once_with(
+            heartbeat="test", environment=environment, timeout=None
+        )
+    else:
+        mock.assert_not_called()
+
+
+@pytest.mark.parametrize("opsduty_base_url", ("https://test.com", "https://opsduty.io"))
+@pytest.mark.parametrize("environment", ("prod", None))
+def test_send_heartbeat_checkin(
+    opsduty_base_url: str, environment: str | None, responses: RequestsMock
+) -> None:
+    """Test send_heartbeat_checkin."""
+
+    heartbeat = "test"
+
+    settings.OPSDUTY_BASE_URL = opsduty_base_url
+
+    url = f"{opsduty_base_url}/heartbeats/checkin/{heartbeat}/"
+    if environment is not None:
+        url += f"{environment}/"
+
+    # The test will fail if we don't execute this request.
+    responses.add(responses.GET, url=url, status=202)
+
+    send_heartbeat_checkin(heartbeat=heartbeat, environment=environment)
