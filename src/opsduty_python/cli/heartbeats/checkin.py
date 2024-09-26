37708d8d0a5746ba25ef51ef3a1cc06bd76e9a30
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2024-09-26 10:41:00 +0200

    Make it possible to send checkin by service-id and name

--- src/opsduty_python/cli/heartbeats/checkin.py
+++ src/opsduty_python/cli/heartbeats/checkin.py
@@ -1,13 +1,46 @@
 import click
+import httpx
 import structlog
+from opsduty_client.api.heartbeats import (
+    opsduty_api_v1_heartbeats_healthcheck_environment_checkin,
+)
+from opsduty_client.errors import UnexpectedStatus
 
+from opsduty_python.cli.mutex import Mutex
+from opsduty_python.cli.utils import (
+    get_client,
+)
 from opsduty_python.heartbeats import send_heartbeat_checkin as _send_heartbeat_checkin
+from opsduty_python.settings import settings
 
 logger = structlog.get_logger()
 
 
 @click.command(help="Send a heartbeat checkin to OpsDuty.")
-@click.argument("heartbeat", type=str, envvar="HEARTBEAT")
+@click.option(
+    "--heartbeat-id",
+    type=str,
+    envvar="HEARTBEAT_ID",
+    help="Heartbeat ID of the heartbeat you want to checkin. ",
+    cls=Mutex,
+    not_required_if=["service_id", "name"],
+)
+@click.option(
+    "--service-id",
+    type=int,
+    envvar="SERVICE_ID",
+    help="Use service id and name to lookup heartbeat instead of the heartbeat id. Authentication required. ",
+    cls=Mutex,
+    not_required_if=["heartbeat_id"],
+)
+@click.option(
+    "--name",
+    type=str,
+    envvar="NAME",
+    help="Use service id and name to lookup heartbeat instead of the heartbeat id. Authentication required. ",
+    cls=Mutex,
+    not_required_if=["heartbeat_id"],
+)
 @click.option(
     "--environment",
     "-e",
@@ -18,24 +51,55 @@ logger = structlog.get_logger()
     envvar="ENV",
     show_envvar=True,
 )
-@click.option(
-    "--timeout",
-    type=int,
-    required=True,
-    default=10,
-    help="Specify the request timeout to OpsDuty.",
-)
 @click.pass_context
 def checkin(
-    ctx: click.Context, *, heartbeat: str, environment: str | None, timeout: int
+    ctx: click.Context,
+    *,
+    heartbeat_id: str | None,
+    service_id: int | None,
+    name: str | None,
+    environment: str | None,
 ) -> None:
-    logger.debug(
-        "Sending heartbeat checkin", heartbeat=heartbeat, environment=environment
-    )
 
-    success = _send_heartbeat_checkin(
-        heartbeat=heartbeat, environment=environment, timeout=timeout
-    )
+    if heartbeat_id:
+        logger.debug(
+            "Sending heartbeat checkin", heartbeat=heartbeat_id, environment=environment
+        )
 
-    if not success:
-        ctx.fail("Could not send heartbeat checkin to OpsDuty.")
+        success = _send_heartbeat_checkin(
+            heartbeat=heartbeat_id,
+            environment=environment,
+            timeout=settings.REQUEST_TIMEOUT,
+        )
+
+        if not success:
+            ctx.fail("Could not send heartbeat checkin to OpsDuty.")
+
+        return
+
+    if service_id and name:
+        logger.debug(
+            "Sending heartbeat checkin",
+            service_id=service_id,
+            name=name,
+            environment=environment,
+        )
+
+        if not environment:
+            ctx.fail("Environment is required when --service-id and --name is used.")
+            return
+
+        client = get_client(ctx, require_authentication=True)
+
+        try:
+            opsduty_api_v1_heartbeats_healthcheck_environment_checkin.sync_detailed(
+                client=client, service_id=service_id, name=name, environment=environment
+            )
+        except (UnexpectedStatus, httpx.TimeoutException):
+            ctx.fail("Could not send heartbeat checkin to OpsDuty.")
+
+        return
+
+    ctx.fail(
+        "Either --hearbeat-id or --service-id and --name is required to lookup heartbeat."
+    )
